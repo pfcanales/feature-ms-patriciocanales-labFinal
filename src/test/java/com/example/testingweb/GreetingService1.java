d7c4d64bcd0dd6224bf6c2b09b27ba274d25deb9
pfcanales <pfcanales@gmail.com>
2023-09-22 14:33:28 -0300

    Update GreetingService1.java

--- src/test/java/com/example/testingweb/GreetingService1.java
+++ src/test/java/com/example/testingweb/GreetingService1.java
@@ -28,7 +28,6 @@ public class GreetingService1 {
 	private GreetingService service;
 
 	@Test
-	@Test
 	public void shouldReturnDefaultMessage() throws Exception {
 		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
 				.andExpect(content().string(containsString("Hello, World")));
