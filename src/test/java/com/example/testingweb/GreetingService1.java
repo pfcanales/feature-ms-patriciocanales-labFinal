2c7d8ab951583860242fcc2400a9e6d6fcbfc959
pfcanales <pfcanales@gmail.com>
2023-09-22 13:47:46 -0300

    Update and rename GreetingService.java to GreetingService1.java

--- src/test/java/com/example/testingweb/GreetingService1.java
+++ src/test/java/com/example/testingweb/GreetingService1.java
@@ -15,7 +15,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(GreetingController.class)
-public class GreetingService {
+public class GreetingService1 {
 
 	@Autowired
 	private MockMvc mockMvc;
