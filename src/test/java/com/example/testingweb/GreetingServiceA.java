c1e93bc5466b39d504bff05aae309350ff2d7797
pfcanales <pfcanales@gmail.com>
2023-09-22 15:07:58 -0300

    Update GreetingServiceA.java

--- src/test/java/com/example/testingweb/GreetingServiceA.java
+++ src/test/java/com/example/testingweb/GreetingServiceA.java
@@ -1,36 +1,11 @@
 package com.example.testingweb;
-
-import static org.hamcrest.Matchers.containsString;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.jupiter.api.Test;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
-
-//@WebMvcTest(GreetingController.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-class GreetingServiceA {
-
-	@Autowired
-	private MockMvc mockMvc;
-
-	@MockBean
-	private GreetingService service;
-
+import static org.assertj.core.api.Assertions.assertThat;
+public class GreetingServiceTest {
 	@Test
-	void greetingShouldReturnMessageFromService() throws Exception {
-		when(service.greet()).thenReturn("Hello, World");
-		this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
-				.andExpect(content().string(containsString("Hello, World")));
+	public void test1() {
+		GreetingService greetingService = new GreetingService();
+		assertThat(greetingService.greet()).contains("Hello, World");
+	
 	}
 }
